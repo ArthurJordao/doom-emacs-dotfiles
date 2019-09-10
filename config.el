0a0f60c6315b4c74f90f37d7f294f72758ca2333
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2019-09-10 20:38:11 -0300

    Fix indentation

--- config.el
+++ config.el
@@ -4,9 +4,9 @@
 (setq
  projectile-project-search-path '("~/dev/nu/"))
 
- (let ((nudev-emacs-path "~/dev/nu/nudev/ides/emacs/"))
-   (when (file-directory-p nudev-emacs-path)
-     (add-to-list 'load-path nudev-emacs-path)
-     (require 'nu)))
+(let ((nudev-emacs-path "~/dev/nu/nudev/ides/emacs/"))
+  (when (file-directory-p nudev-emacs-path)
+    (add-to-list 'load-path nudev-emacs-path)
+    (require 'nu)))
 
 (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
