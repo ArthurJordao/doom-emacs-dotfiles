6588cb2c0524e577db9f275486bf7563f09a7de8
ArthurJordao <arthurbernardijordao@gmail.com>
2019-09-10 15:10:56 -0300

    Activate lispy mode automatically

--- config.el
+++ config.el
@@ -3,3 +3,10 @@
 ;; Place your private configuration here
 (setq
  projectile-project-search-path '("~/dev/nu/"))
+
+ (let ((nudev-emacs-path "~/dev/nu/nudev/ides/emacs/"))
+   (when (file-directory-p nudev-emacs-path)
+     (add-to-list 'load-path nudev-emacs-path)
+     (require 'nu)))
+
+(add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
