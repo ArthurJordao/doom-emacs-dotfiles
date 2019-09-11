53436290f7cd6a1816c368771d21e9908c835ea4
ArthurJordao <arthurbernardijordao@gmail.com>
2019-09-11 10:26:41 -0300

    Increase font size and window frame size

--- config.el
+++ config.el
@@ -2,7 +2,8 @@
 
 ;; Place your private configuration here
 (setq
- projectile-project-search-path '("~/dev/nu/"))
+ projectile-project-search-path '("~/dev/nu/")
+ doom-font (font-spec :family "Fira Code" :size 16))
 
 (let ((nudev-emacs-path "~/dev/nu/nudev/ides/emacs/"))
   (when (file-directory-p nudev-emacs-path)
@@ -10,3 +11,7 @@
     (require 'nu)))
 
 (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+
+(add-to-list 'default-frame-alist '(height . 48))
+
+(add-to-list 'default-frame-alist '(width . 160))
