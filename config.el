0e936999b26504d93e24ec37cc87bbad1e690260
ArthurJordao <arthurbernardijordao@gmail.com>
2019-09-29 11:40:32 -0300

    Add lispy code evaluation

--- config.el
+++ config.el
@@ -12,6 +12,11 @@
 
 (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
 
+(defun conditionally-enable-lispy ()
+  (when (eq this-command 'eval-expression)
+    (lispy-mode 1)))
+(add-hook 'minibuffer-setup-hook 'conditionally-enable-lispy)
+
 (add-to-list 'default-frame-alist '(height . 48))
 
 (add-to-list 'default-frame-alist '(width . 160))
